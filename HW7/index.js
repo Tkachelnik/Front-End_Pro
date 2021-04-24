ff5fe8636b66fe802adbf9afd87b9aa13a3d01a7
Alexandr Tkachuk <sasha2812tk@gmail.com>
2021-04-24 12:34:02 +0300

    Edit HW7

--- HW7/index.js
+++ HW7/index.js
@@ -1,18 +1,32 @@
 let button = document.getElementById("sendBtn");
-let index = 0;
+//let index = 0;
 let msgArea = document.getElementById("msgArea");
 let input = document.getElementById("inputArea");
 function msgAdding() {
-    if (index == 15) {
-        let temp = msgArea.getElementsByTagName('div')[0];
-        temp.remove();
-        index--;
-    }
+    // if (index == 15) {
+    //     let temp = msgArea.getElementsByTagName('div')[0];
+    //     temp.remove();
+    //     index--;
+    // }
     let userMessage = input.value;
     input.value = "";
+    button.setAttribute('disabled', 'disabled');
     let messageDiv = document.createElement('div');
-    msgArea.appendChild(messageDiv);
     messageDiv.prepend(userMessage);
-    index++;
+    msgArea.appendChild(messageDiv);
+    messageDiv.scrollIntoView({  block: 'end'});
+    input.focus();
+   // index++;
 }
 button.addEventListener('click', msgAdding);
+
+input.addEventListener('input', updateValue);
+
+function updateValue(e) {
+    if(e.target.value){
+        button.removeAttribute('disabled');
+    }
+    else{
+        button.setAttribute('disabled', 'disabled');
+    }
+}
